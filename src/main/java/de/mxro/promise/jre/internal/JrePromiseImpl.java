792362dfc31386bff807fbf94646cdd37221ca9e
mxro <mxro@nowhere.com>
2015-01-12 07:59:43 +1100

    Merging Promise Implementations Autocommit Mon Jan 12 07:59:43 EST 2015

--- src/main/java/de/mxro/promise/jre/internal/JrePromiseImpl.java
+++ src/main/java/de/mxro/promise/jre/internal/JrePromiseImpl.java
@@ -1,56 +1,58 @@
 package de.mxro.promise.jre.internal;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
 import de.mxro.async.Operation;
 import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.async.jre.Async;
 import de.mxro.promise.internal.PromiseImpl;
 
 public class JrePromiseImpl<ResultType> extends PromiseImpl<ResultType> {
 
-	@Override
-	public ResultType get() {
+    @Override
+    public ResultType get() {
 
-		ResultType result = super.get();
+        final ResultType result = super.get();
 
-		if (result != null) {
-			return result;
-		}
+        if (result != null) {
+            return result;
+        }
 
-		final CountDownLatch latch = new CountDownLatch(1);
+        Async.waitFor(new Operation<ResultType>() {
 
-		apply(new ValueCallback<ResultType>() {
+            @Override
+            public void apply(final ValueCallback<ResultType> callback) {
+                JrePromiseImpl.this.apply(callback);
+            }
+        });
 
-			@Override
-			public void onFailure(Throwable t) {
-				latch.countDown();
-			}
+        return get();
 
-			@Override
-			public void onSuccess(ResultType value) {
-				latch.countDown();
-			}
-		});
+        /*
+         * final CountDownLatch latch = new CountDownLatch(1);
+         * 
+         * 
+         * 
+         * apply(new ValueCallback<ResultType>() {
+         * 
+         * @Override public void onFailure(Throwable t) { latch.countDown(); }
+         * 
+         * @Override public void onSuccess(ResultType value) {
+         * latch.countDown(); } });
+         * 
+         * try {
+         * 
+         * latch.await(320000, TimeUnit.MILLISECONDS); } catch (final
+         * InterruptedException e) { throw new RuntimeException(e); }
+         * 
+         * if (latch.getCount() > 0) { throw new RuntimeException(
+         * "Get call could not be completed in 320 s timeout."); }
+         * 
+         * return get();
+         */
 
-		try {
+    }
 
-			latch.await(320000, TimeUnit.MILLISECONDS);
-		} catch (final InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-
-		if (latch.getCount() > 0) {
-			throw new RuntimeException(
-					"Get call could not be completed in 320 s timeout.");
-		}
-
-		return get();
-
-	}
-
-	public JrePromiseImpl(Operation<ResultType> asyncPromise) {
-		super(asyncPromise);
-	}
+    public JrePromiseImpl(final Operation<ResultType> asyncPromise) {
+        super(asyncPromise);
+    }
 
 }
