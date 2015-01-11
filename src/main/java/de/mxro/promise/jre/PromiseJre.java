f08da12311fcaca68c80648d7815f5465e8e2c3d
mxro <mxro@nowhere.com>
2015-01-11 11:23:22 +1100

    Moving promise implementation into seperate project Autocommit Sun Jan 11 11:23:22 EST 2015

--- src/main/java/de/mxro/promise/jre/PromiseJre.java
+++ src/main/java/de/mxro/promise/jre/PromiseJre.java
@@ -17,6 +17,10 @@ import de.mxro.async.promise.PromiseFactory;
 
 public class PromiseJre {
 
+    public static <ResultType> ResultType get(final Deferred<ResultType> deferred) {
+        return new JrePromiseImpl<ResultType>(deferred).get();
+    }
+
     public static <T> List<Object> parallel(final List<Promise<T>> promises) {
         return parallel(promises.toArray(new Promise[0]));
     }
