81d92ca75641791e3e40545f8116850f204767bf
mxro <mxro@nowhere.com>
2015-01-11 14:26:06 +1100

    Moving promise implementation into seperate project Autocommit Sun Jan 11 14:26:06 EST 2015

--- src/main/java/de/mxro/promise/helper/P.java
+++ src/main/java/de/mxro/promise/helper/P.java
@@ -3,6 +3,14 @@ package de.mxro.promise.helper;
 import de.mxro.async.Operation;
 import de.mxro.fn.Closure;
 
+/**
+ * <p>
+ * Type for variables which hold promises.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ * @param <ResultType>
+ */
 public interface P<ResultType> extends Operation<ResultType> {
 
     public ResultType get();
