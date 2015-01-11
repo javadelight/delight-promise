881fc4d6ca6c7acda54dbc186c9be2f27349171e
mxro <mxro@nowhere.com>
2015-01-12 09:04:43 +1100

    Merging Promise Implementations Autocommit Mon Jan 12 09:04:43 EST 2015

--- src/main/java/de/mxro/promise/internal/PromiseImpl.java
+++ src/main/java/de/mxro/promise/internal/PromiseImpl.java
@@ -95,8 +95,10 @@ public class PromiseImpl<ResultType> implements Promise<ResultType> {
                 final List<ValueCallback<ResultType>> cachedCalls;
                 synchronized (failureCache) {
 
-                    assert failureCache.get() == null : "Cached exception already set for operation [" + asyncPromise
-                            + "]. Failure: " + failureCache.get();
+                    // assert failureCache.get() == null :
+                    // "Cached exception already set for operation [" +
+                    // asyncPromise
+                    // + "]. Failure: " + failureCache.get();
                     synchronized (resultCache) {
                         resultCache.set(value);
 
