a7611a732f1f81646baa96b4d6b8b8de129abdb8
mxro <mxro@nowhere.com>
2015-01-12 12:32:29 +1100

    Merging Promise Implementations Autocommit Mon Jan 12 12:32:29 EST 2015

--- src/main/java/de/mxro/promise/jre/Promises.java
+++ src/main/java/de/mxro/promise/jre/Promises.java
@@ -3,8 +3,6 @@ package de.mxro.promise.jre;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import de.mxro.async.AsyncCommon;
 import de.mxro.async.AsyncFunction;
@@ -57,61 +55,43 @@ public class Promises {
     @SuppressWarnings("rawtypes")
     public static List<Object> parallel(final Promise... promises) {
 
+        return Async.waitFor(new Operation<List<Object>>() {
 
-        Async.waitFor(new Operation<List<Object>> {
-
-        });
-
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        AsyncCommon.map(Arrays.asList(promises), new AsyncFunction<Promise, Object>() {
-
-            @SuppressWarnings("unchecked")
             @Override
-            public void apply(final Promise input, final ValueCallback<Object> callback) {
-                input.apply(new ValueCallback<Object>() {
+            public void apply(final ValueCallback<List<Object>> callback) {
+                AsyncCommon.map(Arrays.asList(promises), new AsyncFunction<Promise, Object>() {
+
+                    @SuppressWarnings("unchecked")
+                    @Override
+                    public void apply(final Promise input, final ValueCallback<Object> callback) {
+                        input.apply(new ValueCallback<Object>() {
+
+                            @Override
+                            public void onFailure(final Throwable t) {
+                                callback.onFailure(t);
+                            }
+
+                            @Override
+                            public void onSuccess(final Object value) {
+                                callback.onSuccess(value);
+                            }
+                        });
+                    }
+                }, new ListCallback<Object>() {
+
+                    @Override
+                    public void onSuccess(final List<Object> value) {
+                        callback.onSuccess(value);
+                    }
 
                     @Override
                     public void onFailure(final Throwable t) {
                         callback.onFailure(t);
                     }
-
-                    @Override
-                    public void onSuccess(final Object value) {
-                        callback.onSuccess(value);
-                    }
                 });
             }
-        }, new ListCallback<Object>() {
 
-            @Override
-            public void onSuccess(final List<Object> value) {
-                latch.countDown();
-            }
-
-            @Override
-            public void onFailure(final Throwable t) {
-                latch.countDown();
-            }
         });
-
-        try {
-            latch.await(120000, TimeUnit.MILLISECONDS);
-        } catch (final InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        if (latch.getCount() > 0) {
-            throw new RuntimeException("Parallel operation was not completed in timeout.");
-        }
-
-        final List<Object> res = new ArrayList<Object>(promises.length);
-
-        for (final Promise p : promises) {
-            res.add(p.get());
-        }
-
-        return res;
 
     }
 
